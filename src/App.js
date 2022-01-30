5e503aaa932402864d7fd63b34a796eb172a4554
ElenaTkacheva <79642915+ElenaTkacheva@users.noreply.github.com>
2022-01-30 14:42:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
+
 function App() {
+
+  const [hotels, setHotels] = useState(data);
+  const [showMore, setShowMore] = useState(false);
+
+  const removeHotel = (id) => {
+    let newHotels = hotels.filter((hotel) => hotel.id !== id);
+    setHotels(newHotels);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>NYS TOP {hotels.length} HOTELS</h1>
+      </div>
+      { hotels.map ((hotel => {
+        const { id, hotelName, description, image, source } = hotel;
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>
+                {id} - {hotelName}
+              </h2>
+            </div>
+            <div className="container">
+              <p>
+                {showMore ? description : description.substring(0, 260) + "..."}
+                <button onClick={() => setShowMore(!showMore)}>
+                  {showMore ? "Show less" : "Show more"}git init
+                </button>
+              </p>
+            </div>
+            <div className="container">
+              <img src={image} width="500px" />
+            </div>
+            <div className="container">
+              <p className="source">{source}</p>
+            </div>
+            <div className="container">
+              <button className="btn" onClick={() => removeHotel(id)}>
+                REMOVE
+              </button>
+            </div>
+          </div>
+        );
+      }))}
     </div>
   );
 }
